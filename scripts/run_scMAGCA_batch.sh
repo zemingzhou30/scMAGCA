0da12276275ce10e2b834f334904c1ed59317641
zeming zhou <95676270+zemingzhou30@users.noreply.github.com>
2025-04-27 20:49:05 +0800

    Update run_scMAGCA_batch.sh

--- scripts/run_scMAGCA_batch.sh
+++ scripts/run_scMAGCA_batch.sh
@@ -1,4 +1,4 @@
 ### multiBatch
 f=GSE164378
 echo "Run GSE164378"
-python -u -W ignore ../src/run_scMAGCA_batch.py --n_clusters 31 --n_batches 2 --data_file $f --alpha 1 --beta 1 --gama 0.01 --filter2
+python -u -W ignore ../scMAGCA/run_scMAGCA_batch.py --n_clusters 31 --n_batches 2 --data_file $f --alpha 1 --beta 1 --gama 0.01 --filter2
