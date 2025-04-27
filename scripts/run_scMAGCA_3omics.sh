434474b4a8455ed07c18530a47f0bb34d1fcb823
zeming zhou <95676270+zemingzhou30@users.noreply.github.com>
2025-04-27 20:48:47 +0800

    Update run_scMAGCA_3omics.sh

--- scripts/run_scMAGCA_3omics.sh
+++ scripts/run_scMAGCA_3omics.sh
@@ -1,4 +1,4 @@
 ### three-omics 
 f=GSE158013
 echo "Run GSE158013"
-python -u -W ignore ../src/run_scMAGCA_3omics.py --data_file $f --filter2 --filter3 --f2 2000 --f3 2000 --ad_out 32
+python -u -W ignore ../scMAGCA/run_scMAGCA_3omics.py --data_file $f --filter2 --filter3 --f2 2000 --f3 2000 --ad_out 32
